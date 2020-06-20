f79e64fe6dce8c29c7c77165ce8e681ae911c5fa
家子智成 <>
2020-06-20 19:24:33 +0900

    銃のモデル設定

--- Assets/0_MyAssets/Scripts/Game/ItemController.cs
+++ Assets/0_MyAssets/Scripts/Game/ItemController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 public enum ItemType
 {
     TimeInterval,
@@ -10,6 +11,11 @@ public class ItemController : MonoBehaviour
 {
     [SerializeField] ItemType itemType;
     public ItemType ItemType => itemType;
+    Tween rotateTween;
+    void Start()
+    {
+        rotateTween = transform.DORotate(new Vector3(0, 360, 0), 2).SetEase(Ease.Linear).SetLoops(-1).SetRelative();
+    }
     void OnTriggerEnter(Collider other)
     {
         GetItem(other);
@@ -31,5 +37,6 @@ public class ItemController : MonoBehaviour
                 break;
         }
         gameObject.SetActive(false);
+        rotateTween.Kill();
     }
 }
