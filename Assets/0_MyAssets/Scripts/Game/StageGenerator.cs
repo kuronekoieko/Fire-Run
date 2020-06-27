79a0ceca3c216a316bc24cbacfa7a50ad1d5616b
家子智成 <>
2020-06-27 15:59:14 +0900

    オブジェクト配置調整

--- Assets/0_MyAssets/Scripts/Game/StageGenerator.cs
+++ Assets/0_MyAssets/Scripts/Game/StageGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class StageGenerator : MonoBehaviour
 {
@@ -8,13 +9,19 @@ public class StageGenerator : MonoBehaviour
     public void Generate()
     {
         int stageIndex = 0;
-        ObjInfo[,] stageDatas = Variables.stageDatas[stageIndex];
-        for (int iz = 0; iz < stageDatas.GetLength(0); iz++)
+        ObjInfo[,] stageData = Variables.stageDatas[stageIndex];
+        for (int iz = 0; iz < stageData.GetLength(0); iz++)
         {
-            Debug.Log(iz + " ====================");
-            for (int ix = 0; ix < stageDatas.GetLength(1); ix++)
+            // Debug.Log(iz + " ====================");
+            float offset = 2.1f;
+            for (int ix = 0; ix < stageData.GetLength(1); ix++)
             {
-                Debug.Log(stageDatas[iz, ix].key);
+                //Debug.Log(stageDatas[iz, ix].key);
+                Vector3 pos = new Vector3(-offset * 2 + (float)ix * offset, offset / 2f, iz * offset);
+                StageGimmick gimmick = StageGimmicksSO.i.gimmicks.Where(g => g.key == stageData[iz, ix].key).FirstOrDefault();
+                if (gimmick.gimmickObj == null) { continue; }
+                Instantiate(gimmick.gimmickObj, pos, Quaternion.identity);
+
             }
         }
     }
